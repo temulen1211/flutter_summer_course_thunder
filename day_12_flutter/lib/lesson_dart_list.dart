ce23ff3962bfb40dd64b35ee2060372f968bef0e
Thunder-8 <thunder-8@Admin-8s-iMac.local>
2023-07-28 20:06:16 +0800

    day_12

---
+++ day_12_flutter/lib/lesson_dart_list.dart
@@ -0,0 +1,11 @@
+
+var zahiinBaraa = ['hello', 'tort', 'apple', 42, true ];
+
+List<int> zahiinToo = [1, 2, 3, 4, 5, 7];
+var tortuud = <String>['pie', 'tort', 'pinetort', 'donut'];
+
+void main() {
+  print(zahiinBaraa);
+  print(zahiinToo);
+  print(tortuud[3]);
+}
---
+++ day_12_flutter/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:day_12_flutter/profile_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: ProfileScreen()));
+}
